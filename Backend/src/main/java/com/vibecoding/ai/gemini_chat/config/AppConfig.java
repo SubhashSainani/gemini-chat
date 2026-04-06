79edb6a9888e14186a207c09a1c8727686e60bdf
SubhashSainani <subhashsainani4@gmail.com>
2026-04-06 13:46:57 +1000

    update code

--- Backend/src/main/java/com/vibecoding/ai/gemini_chat/config/AppConfig.java
+++ Backend/src/main/java/com/vibecoding/ai/gemini_chat/config/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class AppConfig {
 
+    // Test code
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
